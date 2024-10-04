58a32f9e3f4582453089612c95b8c2414a45af9a
prawinbabub73 <pbaskaran@onemagnify.com>
2024-10-04 18:49:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,9 @@ if ingredient_list:
     #if ingredient_string:
         #session.sql(my_insert_stmt).collect()
         #st.success('Your Smoothie is ordered!', icon="✅")
+
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
